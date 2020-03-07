3e4f6ce63bfcf9bc2f3a92e43e5df08f2cf90983
Marlon Leite <marlonleite@gmail.com>
2020-03-07 14:14:37 -0300

    Change fields to not required

--- addresses/models.py
+++ addresses/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Address(models.Model):
     street = models.CharField(max_length=150)
-    number = models.IntegerField()
-    neighborhood = models.CharField(max_length=50)
+    number = models.IntegerField(null=True, blank=True)
+    neighborhood = models.CharField(max_length=50, blank=True, null=True)
     city = models.CharField(max_length=100)
     federated_state = models.CharField(max_length=2)
     zip_code = models.CharField(max_length=8)
