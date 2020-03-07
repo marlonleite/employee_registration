d078ac230b5003a987cee0aee5114e8d68d727a5
Marlon Leite <marlonleite@gmail.com>
2020-03-07 13:49:41 -0300

    Created Address model

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=150)
+    number = models.IntegerField()
+    neighborhood = models.CharField(max_length=50)
+    city = models.CharField(max_length=100)
+    federated_state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=8)
+
+    def __str__(self):
+        return "{} {} {} - {}-{} {}".format(
+            self.street,
+            self.number,
+            self.neighborhood,
+            self.city,
+            self.federated_state,
+            self.zip_code,
+        )
--- employees/models.py
+++ employees/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from addresses.models import Address
+
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     document = models.CharField(max_length=11)
     email = models.EmailField()
+    address = models.ForeignKey(
+        Address, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
