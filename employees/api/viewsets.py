cd0177a45a235158359163f794a932dd657cfd25
Marlon Leite <marlonleite@gmail.com>
2020-03-08 13:01:38 -0300

    Update EmployeeViewSet with http method allowed

--- employees/api/viewsets.py
+++ employees/api/viewsets.py
@@ -5,5 +5,9 @@ from ..models import Employee
 
 
 class EmployeeViewSet(ModelViewSet):
+    """
+    EmployeeViewSet :GET, POST, PATCH, DEL
+    """
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
