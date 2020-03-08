d8ac6321f023364d0cf7ac0f5e0e003824e1a4da
Marlon Leite <marlonleite@gmail.com>
2020-03-08 18:49:05 -0300

    Add support to nested multipart renderer

--- employee_registration/settings.py
+++ employee_registration/settings.py
@@ -126,8 +126,9 @@ GOCEP_URL = config('GOCEP_URL')
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.MultiPartRenderer',
         'rest_framework.renderers.JSONRenderer',
-    ]
+    ],
 }
 
 LOGGING = {
